eaa318482a6034bf64594c3d73e7c20d6c186289
Gabriel Saillard <gabriel@saillard.dev>
2020-04-04 11:50:20 +0200

    feat(cms): barebone text editor component from CKEditor

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,15 @@
 import * as React from 'react';
+import dynamic from 'next/dynamic';
 
-const deadline = new Date('April 16, 2020 15:00:00 UTC+1').getTime();
+const Editor = dynamic(() => import('../components/editor.js'), {
+	ssr: false
+});
 
-class Index extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			s: 'XX',
-			m: 'XX',
-			h: 'XX',
-			d: 'XX'
-		};
-	}
-
-	componentDidMount() {
-		this.updateClock();
-		this.timer = setInterval(() => {
-			this.updateClock();
-		}, 1000);
-	}
-
-	updateClock() {
-		const now = Date.now();
-		if (deadline < now) {
-			this.setState({
-				s: '00',
-				m: '00',
-				h: null,
-				d: null
-			});
-		} else {
-			let timer = Math.floor((deadline - now) / 1000);
-			const s = timer % 60;
-			timer = (timer - s) / 60;
-			const m = timer % 60;
-			timer = (timer - m) / 60;
-			const h = timer % 24;
-			const d = (timer - h) / 24;
-
-			this.setState({
-				s: (s.toString().length === 2) ? s.toString() : '0' + s,
-				m: (m.toString().length === 2) ? m.toString() : '0' + m,
-				h: h.toString(),
-				d: d.toString()
-			});
-		}
-	}
-
+export default class Index extends React.Component {
 	render() {
 		return (
 			<div className="root">
-				<h1>
-					{this.state.d ? this.state.d + ':' : ''}
-					{this.state.h ? this.state.h + ':' : ''}
-					{this.state.m}:{this.state.s}
-				</h1>
+				<Editor/>
 				<style jsx global>{`
 					html, body {
 						margin: 0;
@@ -63,17 +18,10 @@ class Index extends React.Component {
 				</style>
 				<style jsx>{`
 					.root {
-						background: black;
-						color: white;
 						font-family: sans-serif;
 						height: 100vh;
 						width: 100vw;
 						display: flex;
-					}
-
-					h1 {
-						margin: auto;
-						font-size: 3rem;
 					}
 				`}
 				</style>
@@ -81,5 +29,3 @@ class Index extends React.Component {
 		);
 	}
 }
-
-export default Index;
