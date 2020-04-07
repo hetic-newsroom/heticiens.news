9fd723dfec3ccbe88b5d74ba1dc838449f2eed87
Gabriel Saillard <gabriel@saillard.dev>
2020-04-07 16:03:57 +0200

    feat(ui): button component

---
+++ components/button.js
@@ -0,0 +1,77 @@
+export default props => (
+	<button
+		className={
+			(props.icon && 'icon') ||
+			(props.primary && 'primary') ||
+			(props.positive && 'positive') ||
+			(props.negative && 'negative')
+		}
+		type="button"
+		onClick={props.onClick}
+	>
+		{props.children}
+		<style jsx>{`
+			button {
+				--bg: var(--color-light-grey);
+				--fg: var(--color-black);
+
+				-webkit-appearance: none;
+				outline: none;
+				display: block;
+				min-width: 160px;
+				padding: 10px 15px;
+				margin: 10px;
+				cursor: pointer;
+
+				background: var(--bg);
+				color: var(--fg);
+				border: 4px solid var(--bg);
+				border-radius: 4px;
+				box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+				transform: scale(1);
+
+				transition: .2s ease-out;
+			}
+			button:hover {
+				background: var(--fg);
+				color: var(--bg);
+				border-color: var(--fg);
+				box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.2);
+				transform: scale(1.05);
+			}
+			button:active {
+				background: var(--bg);
+				color: var(--fg);
+				transform: scale(1.05);
+			}
+
+			button.icon {
+				min-width: inherit;
+				width: 54px;
+				height: 54px;
+				padding: 0px;
+				border-radius: 15px;
+			}
+
+			button.primary:hover, button.positive:hover, button.negative:hover {
+				border-color: var(--bg);
+			}
+
+			button.primary {
+				--bg: var(--color-black);
+				--fg: var(--color-white);
+			}
+
+			button.positive {
+				--bg: var(--color-positive);
+				--fg: var(--color-white);
+			}
+
+			button.negative {
+				--bg: var(--color-negative);
+				--fg: var(--color-white);
+			}
+		`}
+		</style>
+	</button>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,47 @@
 import Link from 'next/link';
+import GlobalStyles from '../components/global-styles';
+import Button from '../components/button';
 
 export default () => {
 	return (
 		<div className="root">
-			<h1>Welcome to staging.heticiens.news!</h1>
+			<h1>Welcome!</h1>
+			<h3>To <strong>staging.heticiens.news</strong></h3>
+			<h2>Test zone</h2>
+			<h4>Try some stuff out!</h4>
+			<h5>Im so tiny</h5>
+			<h6>say whaaaaat?</h6>
+			<p>
+				Lorem ipsum <strong>dolor</strong> sit <i>amet</i>.<br/>Dolor
+				perspiciatis id molestias eum sed. Autem aliquam magnam
+				animi. Qui quam aut ullam totam vero nobis. Iure architecto esse
+				iste. Aut inventore quam voluptatibus molestiae enim aut. Repellat
+				officia numquam architecto.
+			</p>
 			<Link href="/login">
-				<a>Are you a contributor? Login here!</a>
+				<Button primary>
+					<span>Login</span>
+				</Button>
 			</Link>
-			<style jsx global>{`
-				html, body {
-					margin: 0;
-				}
-			`}
-			</style>
+			<Button>
+				<span>Button</span>
+			</Button>
+			<Button icon>
+				<span>O</span>
+			</Button>
+			<Button positive>
+				<span>Create</span>
+			</Button>
+			<Button negative>
+				<span>Delete</span>
+			</Button>
+
+			<GlobalStyles/>
 			<style jsx>{`
 				.root {
-					font-family: sans-serif;
 					height: 100vh;
 					width: 100vw;
-					display: flex;
-					flex-direction: column;
-					align-items: center;
-					justify-content: center;
+					padding: 30px;
 				}
 			`}
 			</style>
