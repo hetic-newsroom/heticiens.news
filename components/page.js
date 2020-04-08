1330a8e750c6c25bff532dc88acdbb4d70a12db0
Gabriel Saillard <gabriel@saillard.dev>
2020-04-08 18:42:19 +0200

    feat(ui): centered, max-width'ed page contents for best experience

--- components/page.js
+++ components/page.js
@@ -4,13 +4,28 @@ import GlobalStyles from './global-styles';
 export default props => (
 	<main id="pageRoot">
 		<Head title={props.title} description={props.description}/>
-		{props.children}
+		<div id="pageWidthContainer">
+			{props.children}
+		</div>
 		<GlobalStyles/>
 		<style jsx>{`
 			main#pageRoot {
-				width: 100vw;
+				width: 100%;
+				display: flex;
 				min-height: 100vh;
 				overflow-x: hidden;
+			}
+
+			#pageWidthContainer {
+				margin: 0 auto;
+				width: 100%;
+				max-width: 660px;
+			}
+
+			@media (max-width: 660px) {
+				#pageWidthContainer {
+					width: calc(100% - 30px);
+				}
 			}
 		`}
 		</style>
