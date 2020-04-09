ddbd4399348f8b401891bd130325d84be40830f9
Gabriel Saillard <gabriel@saillard.dev>
2020-04-09 12:39:59 +0200

    feat(ui): /articles/[title] route and corresponding API

---
+++ pages/api/article/[title].ts
@@ -0,0 +1,25 @@
+import {NextApiRequest, NextApiResponse} from 'next';
+import Database from '../../../lib/database';
+import {Article} from '../../../lib/data-validator';
+
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+	const query = req.query.title as string;
+	const title = query.replace(/-/g, ' ');
+
+	const db = new Database();
+	let article;
+
+	try {
+		article = await db.borrow('Articles', {
+			title
+		}, 'title') as Article;
+	} catch (_) {
+		res.status(404);
+		res.end();
+		return;
+	}
+
+	res.status(200);
+	res.setHeader('Cache-control', 'public, max-age=172800, must-revalidate');
+	res.json(article);
+};
---
+++ pages/articles/[title].js
@@ -0,0 +1,19 @@
+import fetch from 'isomorphic-unfetch';
+import Page from '../../components/page';
+
+export default props => (
+	<Page>
+		<p>{JSON.stringify(props)}</p>
+	</Page>
+);
+
+export async function getServerSideProps(ctx) {
+	const {title} = ctx.params;
+	const s = (ctx.req.headers.host.includes('localhost')) ? '' : 's';
+	const res = await fetch(`http${s}://${ctx.req.headers.host}/api/article/${title}`);
+	const data = await res.json();
+
+	return {
+		props: data
+	};
+}
