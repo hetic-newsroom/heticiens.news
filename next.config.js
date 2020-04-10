f0015bc83f4cb2f64b606cfa9899c976f11b1442
Gabriel Saillard <gabriel@saillard.dev>
2020-04-10 13:04:11 +0200

    feat(next): add jsdom for server-side dompurify ops

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@ module.exports = {
 	target: 'serverless',
 	env: {
 		stage: (process.env.NODE_ENV === 'production') ? 'prod' : 'staging'
+	},
+	webpack: config => {
+		// Used to fix build errors with jsdom
+		config.externals = [
+			'canvas',
+			// This is used by next.js
+			'@ampproject/toolbox-optimizer'
+		];
+		return config;
 	}
 };
