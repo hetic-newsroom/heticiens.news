fd5c6f053a5bc6057f2d11d1ad4b7016893d98cb
Gabriel Saillard <gabriel@saillard.dev>
2020-04-14 16:08:06 +0200

    feat(ui): login page

--- pages/login.js
+++ pages/login.js
@@ -1,33 +1,24 @@
-import Link from 'next/link';
-
+import Page from '../components/page';
 import LoginForm from '../components/login-form';
 
-export default () => {
-	return (
+export default () => (
+	<Page
+		title="Contributeurs - H|N"
+		description="Accès réservé aux contributeurs"
+	>
 		<div className="login">
-			<h1>Login page</h1>
 			<LoginForm/>
-			<Link href="/">
-				<a>Go home</a>
-			</Link>
-			<style jsx global>{`
-				html, body {
-					margin: 0;
-				}
-			`}
-			</style>
-			<style jsx>{`
-				.login {
-					font-family: sans-serif;
-					display: flex;
-					flex-direction: column;
-					align-items: center;
-					justify-content: center;
-					width: 100vw;
-					height: 100vh;
-				}
-			`}
-			</style>
 		</div>
-	);
-};
+
+		<style jsx>{`
+			.login {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				height: 80vh;
+			}
+		`}
+		</style>
+	</Page>
+);
