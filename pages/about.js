0cae761b0db03d3b28fb63b7f37e1840f74cac80
Gabriel Saillard <gabriel@saillard.dev>
2020-04-17 11:55:05 +0200

    chore(text): missing space in /about

--- pages/about.js
+++ pages/about.js
@@ -10,11 +10,11 @@ export default () => (
 			<h3>Le manifeste</h3>
 
 			<p>
-				<strong>HETIC Newsroom</strong> est un collectif d’étudiants, qui représente l’ensemble des filières et promotions de HETIC. Il se donne pour ambition de raconter leur“expérience HETIC”, par la production de contenus éditoriaux, tous supports et formats confondus.
+				<strong>HETIC Newsroom</strong> est un collectif d’étudiants, qui représente l’ensemble des filières et promotions de HETIC. Il se donne pour ambition de raconter leur “expérience HETIC”, par la production de contenus éditoriaux, tous supports et formats confondus.
 			</p>
 
 			<p>
-				<strong>HETIC Newsroom</strong> est hébergé et accompagné par HETIC. Mais le collectif définit sa propreligne éditoriale. Elle est indépendante de la pédagogie et de la communication institutionnelle. La direction générale de l’école se porte garante de cette indépendance.
+				<strong>HETIC Newsroom</strong> est hébergé et accompagné par HETIC. Mais le collectif définit sa propre ligne éditoriale. Elle est indépendante de la pédagogie et de la communication institutionnelle. La direction générale de l’école se porte garante de cette indépendance.
 			</p>
 
 			<p>
