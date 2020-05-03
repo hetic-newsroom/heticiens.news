96488dd6c428af7c98c6505cff97c3357c06497d
Gabriel Saillard <gabriel@saillard.dev>
2020-05-03 20:21:19 +0200

    feat(api): upload assets to s3 as authenticated user
    see #20

---
+++ pages/api/upload.ts
@@ -0,0 +1,57 @@
+import {NextApiRequest, NextApiResponse} from 'next';
+import Bucket from '../../lib/s3-bucket';
+import {validateToken, AuthFailure} from './auth';
+
+const acceptedImageFormats = [
+	'jpg',
+	'jpeg',
+	'png',
+	'svg',
+	'gif'
+];
+
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+	const token = req.query.token as string;
+
+	if (req.method !== 'POST') {
+		res.status(400);
+		res.end();
+		return;
+	}
+
+	const authResponse = await validateToken(token);
+
+	if (authResponse.statusCode !== 200) {
+		const {statusCode, error} = authResponse as AuthFailure;
+
+		res.status(statusCode);
+		res.json({error});
+		return;
+	}
+
+	const bucket = new Bucket();
+
+	// Upload image
+	const fileExtension = req.body.slice(
+		req.body.indexOf('data:image/') as number + 11,
+		req.body.indexOf(';base64')
+	);
+
+	if (!acceptedImageFormats.includes(fileExtension)) {
+		res.status(400);
+		res.json({
+			error: 'Bad image format'
+		});
+		return;
+	}
+
+	const base64 = req.body.replace(/^.*,/, ''); // Strip metadata
+	const key = await bucket.upload(base64, fileExtension);
+	const url = `https://bucket.heticiens.news/${key as string}`;
+
+	res.status(201);
+	res.setHeader('Cache-Control', 'no-store');
+	res.json({
+		url
+	});
+};
