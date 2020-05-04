85582187938fd186d5e6d960a1e740921d83b869
Joshua <satwa.fr@gmail.com>
2020-05-04 16:08:47 +0200

    test: isolating backoffice issues

---
+++ pages/test/index.js
@@ -0,0 +1,30 @@
+import dynamic from 'next/dynamic';
+import Page from '../../components/page';
+
+const NeedAuth = dynamic(() => import('../../components/need-auth'), {
+	ssr: false
+});
+
+export default () => {
+	return (
+		<NeedAuth>
+			<Page
+				title="Espace contributeur - H|N"
+				description="Accès réservé aux contributeurs"
+			>
+				<div className="contribspace">
+					<h1>Espace contributeur (authentifié)</h1>
+
+					<style jsx>{`
+						.contribspace {
+							width: 100%;
+							max-width: 660px;
+							margin: 0 auto;
+						}
+					`}
+					</style>
+				</div>
+			</Page>
+		</NeedAuth>
+	);
+};
