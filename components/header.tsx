f5f49fde1b55f198c98f9f6f01178063e2273553
Gabriel Saillard <gabriel@saillard.dev>
2021-01-23 00:16:50 +0100

    feat: added responsive grid column mixins

--- components/header.tsx
+++ components/header.tsx
@@ -45,66 +45,74 @@ export default function Header(props: HeaderProps) {
 		initial={{ opacity: 0 }}
 		animate={{ opacity: 1 }}
 	>
-		<motion.div
-			className={styles.menuToggle}
-			onClick={toggleMenu}
-			initial={{
-				opacity: 0,
-				translateY: (menuOpen) ? '-100%' : '100%'
-			}}
-			animate={{
-				opacity: 1,
-				translateY: '0%'
-			}}
-			key={`menu-toggle-${menuOpen}`}
-		>
-			{ (menuOpen) ? <X/> : <Menu/> }
-		</motion.div>
-		<h1>HETIC Newsroom</h1>
-		<p>{ props.route }</p>
-
-		<AnimatePresence>{menuOpen &&
-			<motion.nav
-				className={styles.menu}
-				variants={{
-					hidden: {
-						height: '0vh',
-						transition: {
-							ease: 'anticipate',
-							duration: 0.3
-						}
-					},
-					opened: {
-						height: `calc(100vh - ${headerHeight}px)`,
-						transition: {
-							ease: 'anticipate',
-							staggerChildren: 0.15,
-							staggerDirection: 1
-						}
-					}
+		<div className={styles.barContainer}>
+			<motion.a
+				className={styles.menuToggle}
+				href="#"
+				onClick={toggleMenu}
+				initial={{
+					opacity: 0,
+					translateY: (menuOpen) ? '-100%' : '100%'
 				}}
-				initial="hidden"
-				animate="opened"
-				exit="hidden"
+				animate={{
+					opacity: 1,
+					translateY: '0%'
+				}}
+				key={`menu-toggle-${menuOpen}`}
 			>
-				<ul>
-					{props.categories.map(e => (
-						<motion.li
-							variants={{
-								hidden: { opacity: 0 },
-								opened: { opacity: 1 }
-							}}
-							key={e[1]}
-						>
-							<span>
-								<Link href={e[1] || '#'}>
-									<a>{e[0]}</a>
-								</Link>
-							</span>
-						</motion.li>
-					))}
-				</ul>
-			</motion.nav>
-		}</AnimatePresence>
+				{ (menuOpen) ? <X/> : <Menu/> }
+			</motion.a>
+			<Link href="/"><a>
+				<h1>HETIC Newsroom</h1>
+			</a></Link>
+			<p>{ props.route }</p>
+
+			<AnimatePresence>{menuOpen &&
+				<motion.section
+					className={styles.menu}
+					variants={{
+						hidden: {
+							height: '0vh',
+							transition: {
+								ease: 'anticipate',
+								duration: 0.3
+							}
+						},
+						opened: {
+							height: `calc(100vh - ${headerHeight}px)`,
+							transition: {
+								ease: 'anticipate',
+								staggerChildren: 0.15,
+								staggerDirection: 1
+							}
+						}
+					}}
+					initial="hidden"
+					animate="opened"
+					exit="hidden"
+				>
+					<nav>
+						<ul>
+							{props.categories.map(e => (
+								<motion.li
+									variants={{
+										hidden: { opacity: 0 },
+										opened: { opacity: 1 }
+									}}
+									key={e[1]}
+								>
+									<span>
+										<Link href={e[1] || '#'}>
+											<a>{e[0]}</a>
+										</Link>
+									</span>
+								</motion.li>
+							))}
+						</ul>
+						<div style={{ background: 'red' }}></div>
+					</nav>
+				</motion.section>
+			}</AnimatePresence>
+		</div>
 	</motion.header>
 }
