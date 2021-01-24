51d48afa054501e7530d732f85cbc3b33ff21c88
Gabriel Saillard <gabriel@saillard.dev>
2021-01-24 18:09:15 +0100

    feat: custom Prismic RichText HTML Serializer
    print optimized next/image components instead of bare <img>s, use
    client-side Next router for in-site navigation links

---
+++ lib/prismic-richtext-serializer.tsx
@@ -0,0 +1,35 @@
+import type { ReactNode } from 'react'
+import type { HTMLSerializer } from 'prismic-reactjs'
+import URL from 'url-parse'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Elements } from 'prismic-reactjs'
+import Img, { toImage } from 'types/image'
+
+const htmlSerializer: HTMLSerializer<ReactNode> = (type, element, _, children, key) => {
+
+	switch(type) {
+		case Elements.image: {
+			const img: Img = toImage(element)
+			return <Image
+				{ ...img }
+				className="imagetest"
+				key={key}
+			/>
+		}
+		case Elements.hyperlink: {
+			const { url } = element.data
+			if (url.startsWith('https://heticiens.news') || url.startsWith('/')) {
+				return <Link href={new URL(url).pathname} key={key}>
+					<a>{ children }</a>
+				</Link>
+			} else {
+				return <a href={url} target="_blank" rel="noopener">{ children }</a>
+			}
+		}
+		default:
+			return null
+	}
+}
+
+export default htmlSerializer
