3cccfaaa7031b0a6300454baa765fed87a77dced
Gabriel Saillard <gabriel@saillard.dev>
2021-01-28 15:21:34 +0100

    feat: static paths generation for article pages, future-proof

--- pages/article/[uid].tsx
+++ pages/article/[uid].tsx
@@ -1,15 +1,36 @@
 import type { GetStaticPaths, GetStaticProps } from 'next'
-import { client } from 'lib/prismic'
+import query, { client } from 'lib/prismic'
 import Article, { toArticle, prArticle, prArticleAllFields } from 'types/article'
 import { prCategoryMinFields } from 'types/category'
 import { prAuthorMinFields } from 'types/author'
 import ArticleFullView from 'components/article-full-view'
 
 export const getStaticPaths: GetStaticPaths = async () => {
+	let page = 1
+	let totalPages = 1
+	const articleUids: string[] = []
+	while(page <= totalPages) {
+		const { results, total_pages: newTotalPages } = (await query('document.type', prArticle, {
+			fetch: `${prArticle}.title`, // only fetch a single field, saves time & bandwith
+			pageSize: 100,
+			page
+		}))
+		const uids: string[] = results.map(x => x.uid as string)
+		articleUids.push(...uids)
+
+		page++
+		totalPages = Number(newTotalPages)
+	}
+
+	const paths = []
+	for (const uid of articleUids) {
+		paths.push({
+			params: { uid }
+		})
+	}
+
 	return {
-		paths: [
-			{ params: { uid: 'one' } }
-		],
+		paths,
 		fallback: 'blocking'
 	}
 }
