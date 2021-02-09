fbe67b042a7ad41ac1400cda2593f2c13842c26d
Gabriel Saillard <gabriel@saillard.dev>
2021-02-09 15:22:37 +0100

    feat: columns component: support for gap props

--- components/columns.tsx
+++ components/columns.tsx
@@ -5,18 +5,31 @@ import styles from './columns.module.scss'
 
 const cx = classNames.bind(styles)
 
+type Spacing = 'x-small' | 'small' | 'medium' | 'large'
 export interface ColumnsProps {
 	no: '4'  | '3' | '23' | '2' | '1'
+	gap?: Spacing
+	rowGap?: Spacing
+	columnGap?: Spacing
 	revealAnimation?: boolean
 }
 
-export default function Columns({ no, children, revealAnimation }: PropsWithChildren<ColumnsProps>) {
+export default function Columns({ no, children, gap, rowGap, columnGap, revealAnimation }: PropsWithChildren<ColumnsProps>) {
+	if (typeof gap === 'string') {
+		rowGap = gap
+		columnGap = gap
+	}
+
 	const cns = cx({
 		columns4: (no === '4'),
 		columns3: (no === '3'),
 		columns23: (no === '23'),
 		columns2: (no === '2'),
-		column1: (no === '1')
+		column1: (no === '1'),
+		rowGap: (typeof rowGap === 'string'),
+		columnGap: (typeof columnGap === 'string'),
+		[(rowGap) ? `row-${rowGap}`: '']: (typeof rowGap === 'string'),
+		[(columnGap) ? `column-${columnGap}`: '']: (typeof columnGap === 'string')
 	})
 
 	if (revealAnimation) {
